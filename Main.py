8ee7abe9e4ff4b7950be5489a876a5f57d4f6ae6
urk21cs3028Durgaprasadreddy <109951970+urk21cs3028Durgaprasadreddy@users.noreply.github.com>
2022-09-12 16:21:39 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,13 @@
 from typing import List
-
 def selectionSort(array, size) -> List[int]:
-  # Write your code here
+  for step in range(size):
+    min_idx = step
+    for i in range(step + 1, size):
+      if array[i] < array[min_idx]:
+        min_idx = i
+    (array[step], array[min_idx]) = (array[min_idx], array[step])
+  return array
 
-# Do not change the following code
 input_data = input()
 data = []
 for item in input_data.split(', '):
